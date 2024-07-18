412c9aa0b1a1bb185192aa9cbba6b40a6699d899
newyork <spidermanwin1@gmail.com>
2024-07-18 17:03:29 +0700

    commit(not completed)

--- main/main.c
+++ main/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
 
 void app_main(void)
 {
-    int i = 0;
+    gpio_set_direction(5, GPIO_MODE_OUTPUT);
+    gpio_set_direction(17, GPIO_MODE_INPUT);
+    int button2 = 0;
     while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        button2 = gpio_get_level(GPIO_NUM_17);
+        printf("button 2 = %d\n", button2);
+        if(button2 == 1)
+        {
+            gpio_set_level(5,1);
+        }
+        else
+        {
+            gpio_set_level(5,0);
+        }
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
